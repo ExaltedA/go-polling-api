dbc8e5794da2ef52f97eb4e92f02e988e0eb888a
ExaltedA <aldie1741@gmail.com>
2021-09-09 13:43:57 +0600

    initial add of exisiting files

---
+++ functions/functions.go
@@ -0,0 +1,7 @@
+package functions
+
+import "fmt"
+
+func Greet() {
+	fmt.Println("Greetings, my frined!")
+}
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	lib "go_crud/functions"
+)
+
+
+func main() {
+	fmt.Println("Works!")
+	lib.Greet()
+}
